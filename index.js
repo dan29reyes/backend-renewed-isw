b1392993bcf21a7bed25bfb0380b2b44c0cebb36
Kenneth Daniel Reyes Reyes <98404284+dan29reyes@users.noreply.github.com>
2023-10-05 15:52:23 -0600

    cors try

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ const app = express();
 
 require("dotenv").config();
 
-const allowedOrigins = ["http://localhost:3000"];
+// const allowedOrigins = [,"http://localhost:3000"];
 
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  }
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   }
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 app.use(bodyParser.json());
 
 const adminRouter = require("./Route/admin-routes");
